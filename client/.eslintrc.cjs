ae09567a4b44d707c4a42bd07393e5aceed32534
Arshiya <arshiyah52@gmail.com>
2023-10-09 23:02:42 +0330

    add new packeges and configed

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,10 +8,11 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: {'jsx':true} },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/prop-types": 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
